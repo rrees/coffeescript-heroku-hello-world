f7aa22244fc2861a47c9e4614c6564c62c104317
Robert Rees <robert@wazoku.com>
2012-03-26 13:19:58 +0100

    Styling the app

--- web.coffee
+++ web.coffee
@@ -2,5 +2,29 @@
 port = Number(process.env.PORT or 3000)
 
 hello = require('zappa') port, ->
-	@get '/': 'Hello world'
+	@get '/':  ->
+		@render index : {'name' : 'world'}
 	
+	@view index: ->
+		@title = 'Greetz'
+		
+		h1 "Demo site"
+		p "Hello " + @name
+
+	@view layout: ->
+		doctype 5
+		html ->
+			head ->
+				title @title
+				link rel: "stylesheet", href: "/base.css"
+			body @body
+	
+	@css '/base.css' : '''
+			body {
+				margin-left: auto;
+				margin-right: auto;
+				width:80%;
+				font-family: sans-serif;
+				margin-top: 2em;
+			}
+		'''
--- web.js
+++ web.js
@@ -4,8 +4,39 @@
   port = Number(process.env.PORT || 3000);
 
   hello = require('zappa')(port, function() {
-    return this.get({
-      '/': 'Hello world'
+    this.get({
+      '/': function() {
+        return this.render({
+          index: {
+            'name': 'world'
+          }
+        });
+      }
+    });
+    this.view({
+      index: function() {
+        this.title = 'Greetz';
+        h1("Demo site");
+        return p("Hello " + this.name);
+      }
+    });
+    this.view({
+      layout: function() {
+        doctype(5);
+        return html(function() {
+          head(function() {
+            title(this.title);
+            return link({
+              rel: "stylesheet",
+              href: "/base.css"
+            });
+          });
+          return body(this.body);
+        });
+      }
+    });
+    return this.css({
+      '/base.css': 'body {\n	margin-left: auto;\n	margin-right: auto;\n	width:80%;\n	font-family: sans-serif;\n	margin-top: 2em;\n}'
     });
   });
 
